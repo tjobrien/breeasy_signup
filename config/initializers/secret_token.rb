b41532442bde2c398273263a1a9f1e7e33191472
Tom OB <tobrien999@gmail.com>
2013-10-25 09:40:59 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BreeasySignup::Application.config.secret_token = '951c2425159959714907a45acdffcf8c2b1545c73d97cd529e1a6b7d2d559e0724862ec3d1d5233c2faf5f2127c03e751c1d58f96a9702a1d10aecf6060844cb'
